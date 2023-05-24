c98bba6f19ba60a803573ad896e63497c3402164
walyson <walysonmoises1342@gmail.com>
2023-05-24 00:07:13 -0300

    Order e User criados

---
+++ src/main/java/com/walyCommerce/walycommerce/WalycommerceApplication.java
@@ -0,0 +1,13 @@
+package com.walyCommerce.walycommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalycommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalycommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walyCommerce/walycommerce/WalycommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walyCommerce.walycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalycommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
