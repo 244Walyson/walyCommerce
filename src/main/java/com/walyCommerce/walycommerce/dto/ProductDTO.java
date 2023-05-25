c9f29f92b1c0dbd133eaced59bcbb8b99ebe8a3b
walyson <walysonmoises1342@gmail.com>
2023-05-25 15:28:30 -0300

    bean validation

--- src/main/java/com/walyCommerce/walycommerce/dto/ProductDTO.java
+++ src/main/java/com/walyCommerce/walycommerce/dto/ProductDTO.java
@@ -1,11 +1,20 @@
 package com.walyCommerce.walycommerce.dto;
 
 import com.walyCommerce.walycommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter entre 3 e 80 caractere s")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10, message = "Descrição deve ter no minimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
