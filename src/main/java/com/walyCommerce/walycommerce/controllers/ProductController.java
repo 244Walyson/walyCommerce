93d93618ac0e191910e8060f227cae49f9626159
walyson <walysonmoises1342@gmail.com>
2023-05-25 12:37:31 -0300

    delete product

--- src/main/java/com/walyCommerce/walycommerce/controllers/ProductController.java
+++ src/main/java/com/walyCommerce/walycommerce/controllers/ProductController.java
@@ -43,5 +43,11 @@ public class ProductController {
     public ResponseEntity<ProductDTO> update(@PathVariable Long id,@RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
+
+    }
+    @DeleteMapping(value ="/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id) {
+       service.delete(id);
+       return ResponseEntity.noContent().build();
     }
 }
